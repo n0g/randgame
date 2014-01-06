eb03571ef3322c496aedccac14ec2e26d7d283ca
fbr <some@thing.asdf>
2014-01-06 23:29:24 +0100

    added more functionality, settings option, remove players, input checking, etc

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ randgame
 ========
 
 a small tool to randomly decide the next player in tabletop games
+
+the idea is that randomly choosing turns makes it harder for alliances in strategy games to carry out attacks against other players.
+
+the order of players in one round is shuffled, with an option that one can't have two consecutive turns.
+
+this might break balance in some games, it was not tested yet in a real game.
--- randgame.py
+++ randgame.py
@@ -1,22 +1,32 @@
 import random, cmd, sys
 
-players = []
-no_two = False # True == no two consecutive turns for one player
-#turn
-# todo
-# add no_two functionality
-
 class randgame(cmd.Cmd):
 	intro = "this is randgame"
 	prompt = "randgame # "
-	file = None
 	players = ["asdf", "foo"]
 	turn = 0
+	rounds = 0
 	no_two = False
 	active = False
+	settings = {"no_two" : 0, "voice" : 0}
+	last_player = ""
 	def do_f(self, arg):
-		print arg
+		print self.last_player
 
+	def do_set(self, arg):
+		'set settings. see \"list settings\" for available options'
+		if arg == "" or len(arg.split()) != 2:
+			print "*** syntax: set <key> <value>, where value may be 0 or 1"
+			return
+		setting, value = arg.split()
+		if setting not in self.settings.keys():
+			print '*** unrecognized setting. available settings: {0}'.format(", ".join(self.settings.keys()))
+			return
+		if value not in ['0', '1']:
+			print "*** value must be 0 or 1" 
+			return
+		self.settings[setting] = int(value)
+	
 	def do_start(self, arg):
 		'starts the game'
 		if self.active == False:
@@ -25,74 +35,101 @@ class randgame(cmd.Cmd):
 			self.shuffle()
 			self.do_next(1)
 		else:
-			print "Game already started! use \"next\" instead"
+			print "*** Game already started! use \"next\" instead"
 	
 	def do_next(self, arg):
 		'shows next player'
 		if self.active == False :
-			print "No active game, use \"start\" first"
+			print "*** No active game, use \"start\" first"
 			return
 		print "#"*50
 		print
 		print "{0} Player is {1}".format(("First" if arg==1 else "Next"), self.players[self.turn])
 		print 
 		print "#"*50
+
 		self.turn += 1
 		if self.turn == len(self.players):
+			# save last player in this round
+			self.last_player = self.players[self.turn-1]
 			self.turn = 0
 			self.shuffle()
+			self.rounds += 1
 
 	def do_end(self, arg):
 		'ends current game (but does not exit)'
+		if self.active != True:
+			print "*** no active game!"
+			return
 		self.turn = 0
 		self.active = False
+		print "game ended after {0} rounds!".format(self.rounds)
+		self.rounds = 0
+		
 		
 	def shuffle(self):
-		if no_two == False :
+		if self.settings["no_two"] == 0 :
 			random.shuffle(self.players)
 		else:
-			new_players = [random.choice(players)]
-			while(new_players == last_player):
-				new_players = [random.choice(players)]
-			players.remove(new_players[0])
-			random.shuffle(players)
-			new_players += players
-			players = new_players
+			new_players = [random.choice(self.players)]
+			print "last: {}".format(self.last_player)
+			print "prewhile ",
+			print new_players[0]
+			while(new_players[0] == self.last_player):
+				new_players = [random.choice(self.players)]
+			print "postwhile first player: ",
+			print new_players[0]
+			
+			self.players.remove(new_players[0])
+			random.shuffle(self.players)
+			new_players += self.players
+			self.players = new_players
 				
 	def do_addplayer(self, arg):
 		'add a player to the game'
-		self.players.append(arg)
-		
-	def do_list(self, arg):
-		'list players'
-		print ", ".join(map(str, self.players))
-		
+		if self.active == True:
+			print "*** can't add player during active game"
+			return
+			
+		if arg != "":
+			if arg not in self.players:
+				self.players.append(arg)
+			else:
+				print "*** player already added, please specify a different name"
+		else:
+			print "*** please specify a name"
+			
+	def do_remove(self, arg):
+		'remove a player from the game'
+		if self.active == True:
+			print "*** can't remove player during game"
+			return
+		try:
+			self.players.remove(arg)
+			print "removed player {0} from game".format(arg)
+		except ValueError:
+			print "*** player not in game (check spelling?)"
+
 	
-	def do_e(self, arg):
+	def do_list(self, arg):
+		'list settings or list players'
+		if arg == "settings":
+			print "settings: "
+			for key in self.settings:
+				print "\t{0}\t{1}".format(key, self.settings[key])
+		elif arg == "players":
+			print ", ".join(map(str, self.players))
+		else:
+			print "*** \"list settings\" or \"list players\""
+		
+	def do_q(self, arg):
+		'exit the program'
+		return True
+		
+	def do_exit(self, arg):
 		'exit the program'
 		return True
 	
 if __name__ == '__main__':
     randgame().cmdloop()
-
-#def init():
-
-
-
-
-
-#def main():
-#	init()
-
-	# wait for space
-
-
-	#begin
-	# call next
-	# output result of next
-	# wait for input
-	# 	space: 	next
-	#	enter: 	game won
-	#	esc:	restart or exit
-	# goto begin 
 
